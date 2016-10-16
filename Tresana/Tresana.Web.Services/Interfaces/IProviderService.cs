0ec2314d53427c6cd64395f5146ee7566105fefe
Emiliano Gnocchi <emiliano.gnocchi@gmail.com>
2016-10-16 16:06:58 -0300

    Clase Provider implementada

---
+++ Tresana/Tresana.Web.Services/Interfaces/IProviderService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tresana.Data.Entities;
+
+namespace Tresana.Web.Services.Interfaces
+{
+    public interface IProviderService
+    {
+        Provider GetProviderById(int userId);
+        IEnumerable<Provider> GetAllProviders();
+        int CreateProvider(Provider provider);
+        bool UpdateProvider(int Id, Provider provider);
+        bool DeleteProvider(int Id);
+    }
+}
