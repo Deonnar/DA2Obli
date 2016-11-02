cf7c88e26f2ff96d107b90f418e3c7b2acc63a54
Emiliano Gnocchi <emiliano.gnocchi@gmail.com>
2016-11-02 13:28:56 -0300

    TDD: Minimo codigo para pasar el test agregado

---
+++ Fuentes/TodoPagos/TodoPagos/TodoPagos.Dominio/Entidades.Proveedores/ProveedoresBloqueados.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoPagos.Dominio.Entidades.Proveedores
+{
+
+    [Table("ListaNegra")]
+    public class ProveedoresBloqueados
+    {
+        [Key]
+        [Column("ID")]
+        public int ProveedorBloqueadoId { get; set; }
+
+        public Proveedor Proveedor { get; set; }
+
+        public String MotivoBloqueo { get; set; }
+
+    }
+
+}
