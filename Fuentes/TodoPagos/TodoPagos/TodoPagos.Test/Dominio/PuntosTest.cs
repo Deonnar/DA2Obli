32c78341b545dd1a3e9f17e2eb45f46b42a9bb44
Emiliano Gnocchi <emiliano.gnocchi@gmail.com>
2016-11-01 21:19:52 -0300

    TDD: Se crea el test de Puntos con dos metodos y atributos

---
+++ Fuentes/TodoPagos/TodoPagos/TodoPagos.Test/Dominio/PuntosTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoPagos.Dominio.Entidades.Pagos;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TodoPagos.Dominio.Entidades.Proveedores;
+
+
+namespace TodoPagos.Test.Dominio
+{
+    [TestClass]
+    public class PuntosTest
+    {
+        [TestMethod]
+        public void TestCrearPuntos()
+        {
+            Puntos puntos = new Puntos();
+            puntos.ValorPunto = 100;
+            Assert.Equals(100, puntos.ValorPunto);
+        }
+
+        [TestMethod]
+        public void TestPuntos()
+        {
+            Proveedor p = new Proveedor();
+            Puntos puntos = new Puntos();
+            puntos.listaNegra.add(p);
+            Assert.Equals(100, puntos.ValorPunto);
+        }
+
+    }
+}
