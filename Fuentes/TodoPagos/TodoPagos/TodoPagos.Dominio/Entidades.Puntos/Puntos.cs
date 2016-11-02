1cd0f1cf760d19350c712ce1220a47d665e3f468
Emiliano Gnocchi <emiliano.gnocchi@gmail.com>
2016-11-01 21:21:11 -0300

    TDD: minimo codigo para que el test pase agregado.

---
+++ Fuentes/TodoPagos/TodoPagos/TodoPagos.Dominio/Entidades.Puntos/Puntos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using TodoPagos.Dominio.Entidades.Pagos.Cliente;
+
+namespace TodoPagos.Dominio.Entidades.Puntos
+{
+    public class Puntos
+    {
+        [Key]
+        [Column("ID")]
+        public int PuntosId { get; set; }
+        public string ValorPunto { get; set; }
+
+        public List<Pago> listaPagos { get; set; }
+    }
+}
