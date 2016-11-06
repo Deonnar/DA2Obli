700f753b97148f9ee21942c79a8185797847143a
Emiliano Gnocchi <emiliano.gnocchi@gmail.com>
2016-11-06 14:12:49 -0300

    Se realiza pruebas de eliminar pagos y puntos

--- Fuentes/TodoPagos/TodoPagos/TodoPAgos.RestApi/app/login/loginServicio.js
+++ Fuentes/TodoPagos/TodoPagos/TodoPAgos.RestApi/app/login/loginServicio.js
@@ -3,44 +3,57 @@
 
     var obligatorioApp = angular.module('TodoPagos');
 
-    obligatorioApp.controller('Login.Controller', function ($scope, loginService) {
+    obligatorioApp.service('loginServicio', function ($log, $http) {
+        var loginServicio = this;
 
-        var ctrl = this;
-        ctrl.service = loginService;
+        loginServicio.login = function (u, password) {
+            var promise = $http({
+                url: '/api/auth',
+                method: "POST",
+                data: { 'email': emaildata, 'password': password }
+            })
+             .then(function (response) {
+                 sessionStorage.authToken = response.data.Email;
+                 loginServicio.TypeOfUser().then(function (typeOfUser) {
+                     if (typeOfUser.data == 1) {
+                         location.href = "#/stocks";
+                         sessionStorage.typeOfUser = 1;
+                     }
+                     if (typeOfUser.data == 2) {
+                         location.href = "#/portfolio";
+                         sessionStorage.typeOfUser = 2;
+                     }
 
-        $scope.Login = function () {
-            loginService.login($scope.loginEmail, $scope.loginPassword).then(function () {
-                if (loginService.message)
-                    $scope.showError = loginService.message;
-            });
-            $scope.token = sessionStorage.authToken;
-        };
+                 });
+             },
+             function (response) {
+                 loginServicio.message = response.data.Message;
+             });
+            return promise;
+        }
 
-        $scope.Logout = function () {
-            loginService.logout();
-        };
+        loginServicio.TypeOfUser = function () {
+            var promise = $http.get('/api/auth?mail=' + sessionStorage.authToken)
+             .success(function (result) {
+                 return result;
+             })
+             .error(function (data, status) {
+                 $log.error(data);
+             });
+            return promise;
+        }
 
-        var errorMessagge = loginService.message;
+        loginServicio.logout = function () {
+            sessionStorage.typeOfUser = 0;
+            $http({
+                url: '/api/auth?email=' + sessionStorage.authToken,
+                method: 'DELETE'
+            })
+            sessionStorage.authToken = undefined;
+            location.href = "#/login"
+        }
 
-        $scope.showError = errorMessagge;
-
-        $scope.$watch(function () {
-            return loginService.message;
-        }, function (value) {
-            $scope.showError = value;
-        });
-
-        var token = sessionStorage.authToken;
-
-        $scope.showToken = token;
-
-        $scope.$watch(function () {
-            return sessionStorage.authToken;
-        }, function (value) {
-            $scope.showToken = value;
-        });
-
-        return ctrl;
+        return loginServicio;
     });
 })();
 
